6bb0c93e0af775278681f37d19d480671c065a50
Román Arias David <appdeivid99@gmail.com>
2026-05-28 16:33:07 +0000

    SOLUCION DE LOS EJERCICIOS 1 Y 2

--- mi-app-angular/src/app/pages/ejercicio1/ejercicio1.ts
+++ mi-app-angular/src/app/pages/ejercicio1/ejercicio1.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio1',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './ejercicio1.html',
-  styleUrl: './ejercicio1.css',
+  styleUrl: './ejercicio1.css'
 })
-export class Ejercicio1 {}
+export class Ejercicio1 {
+
+  nombre: string = '';
+  N1: number = 0;
+  N2: number = 0;
+  N3: number = 0;
+  N4: number = 0;
+  suma: number = 0;
+  promedio: number = 0;
+  estadoAcademico: string = '';
+  observacion: string = '';
+  mostrarResultados: boolean = false;
+
+  enviarFormulario() {
+    this.suma = this.N1 + this.N2 + this.N3 + this.N4;
+    this.promedio = parseFloat((this.suma / 4).toFixed(2));
+    
+    if (this.promedio >= 13) {
+      this.estadoAcademico = 'USTED ESTA APROBADO';
+    } else {
+      this.estadoAcademico = 'USTED ESTA DESAPROBADO';
+    }
+
+    if (this.promedio >= 17) {
+      this.observacion = 'Excelente, eres todo un Tigre';
+    } else if (this.promedio >= 13 && this.promedio < 17) {
+      this.observacion = 'Regular, puede mejorar';
+    } else {
+      this.observacion = 'En riesgo, el otro año sera joven';
+    }
+
+    this.mostrarResultados = true;
+  }
+
+  limpiarFormulario() {
+    this.nombre = '';
+    this.N1 = 0;
+    this.N2 = 0;
+    this.N3 = 0;
+    this.N4 = 0;
+    this.suma = 0;
+    this.promedio = 0;
+    this.estadoAcademico = '';
+    this.observacion = '';
+    this.mostrarResultados = false;
+  }
+
+}
--- mi-app-angular/src/app/pages/ejercicio2/ejercicio2.ts
+++ mi-app-angular/src/app/pages/ejercicio2/ejercicio2.ts
@@ -1,9 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio2',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './ejercicio2.html',
   styleUrl: './ejercicio2.css',
 })
-export class Ejercicio2 {}
+export class Ejercicio2 {
+
+  nombre: string = '';
+  correo: string = '';
+  peso: number = 0;
+  altura: number = 0;
+  imc: number | null= null;
+  categoria: string = '';
+  mostrarResultados: boolean = false;
+
+  enviarFormulario() {
+    if (this.altura > 0) {
+      this.imc = parseFloat((this.peso / (this.altura * this.altura)).toFixed(2));
+      
+      if (this.imc < 18.5) {
+        this.categoria = 'Bajo peso';
+      } else if (this.imc >= 18.5 && this.imc <= 24.99) {
+        this.categoria = 'Peso saludable';
+      } else if (this.imc >= 25.0 && this.imc <= 29.99) {
+        this.categoria = 'Sobrepeso';
+      } else {
+        this.categoria = 'Obesidad';
+      }
+      
+      this.mostrarResultados = true;
+    }
+  }
+
+  limpiarFormulario() {
+    this.nombre = '';
+    this.correo = '';
+    this.peso = 0;
+    this.altura = 0;
+    this.imc = null;
+    this.categoria = '';
+    this.mostrarResultados = false;
+  }
+
+}
