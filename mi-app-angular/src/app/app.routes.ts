9546abd603a7784919b57072d3cfde9706a8764e
Román Arias David <appdeivid99@gmail.com>
2026-05-28 07:40:49 +0000

    Actualización de Botones

--- mi-app-angular/src/app/app.routes.ts
+++ mi-app-angular/src/app/app.routes.ts
@@ -2,17 +2,16 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Productos } from './pages/productos/productos';
 import { Registro } from './pages/registro/registro';
+import { Ejercicio1 } from './pages/ejercicio1/ejercicio1';
+import { Ejercicio2 } from './pages/ejercicio2/ejercicio2';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-{ path: 'inicio', component: Inicio },
-{ path: 'productos', component: Productos },
-{ path: 'registro', component: Registro },
-{ path: '**', redirectTo: 'inicio' }
-
-
-
-
-
-
+  { path: 'inicio', component: Inicio },
+  { path: 'productos', component: Productos },
+  { path: 'registro', component: Registro },
+  { path: 'ejercicio1', component: Ejercicio1 },
+  { path: 'ejercicio2', component: Ejercicio2 },
+  
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', redirectTo: 'inicio' }
 ];
